faff7deb0aeae6c1220c97f0cb06c5f38e8e3f80
vishalkumar2024 <vkumarsoni30@gmail.com>
2025-05-16 12:19:00 +0530

    created a file named as implement stack using queue

---
+++ Implement-stack-using-queue.js
@@ -0,0 +1,79 @@
+class Queue {
+    constructor() {
+        this.item = [];
+    }
+
+    push(ele) {
+        return this.item.push(ele);
+    }
+
+    pop() {
+        if (this.isEmpty()) {
+            return "Queue Underflow";
+        }
+        return this.item.shift();
+    }
+
+    atTop() {
+        if (this.size() !== 0)
+            return this.item[0]
+        else return "Queue is empty"
+    }
+
+    size() {
+        return this.item.length;
+    }
+
+    isEmpty() {
+        return (this.size() == 0)
+    }
+}
+
+class Stack {
+    constructor() {
+        this.que = new Queue(); // each stack has its own queue
+    }
+
+    push(item) {
+        let length = this.que.size();
+        this.que.push(item);
+
+        for (let i = 0; i < length; i++) {
+            // this.que.push(this.que.pop());
+            let del = this.que.pop();
+            this.que.push(del)
+        }
+    }
+    pop() {
+        this.que.pop();
+    }
+    atTop() {
+        return this.que.atTop()
+    }
+    isEmpty() {
+        return this.que.isEmpty() == 0
+    }
+    size() {
+        return this.que.size();
+    }
+}
+
+let stk = new Stack();
+
+stk.push(10)
+stk.push(20)
+stk.push(30)
+stk.push(40)
+
+// stk.pop();
+
+// console.log(stk)
+// console.log(stk.atTop())
+// console.log(stk.isEmpty())
+// console.log(stk.size())
+
+while(stk.size()!==0){
+    console.log(stk.atTop());
+    stk.pop();
+}
+//  output: 40, 30, 20, 10
