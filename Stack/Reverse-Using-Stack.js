422069d22179ffa279aac414d457ba0f826a9299
vishalkumar2024 <vkumarsoni30@gmail.com>
2025-05-28 11:18:40 +0530

    modified reverse using stack

--- Stack/Reverse-Using-Stack.js
+++ Stack/Reverse-Using-Stack.js
@@ -1,19 +1,21 @@
 class Solution {
     reverse(s) {
-        let splitS = s.split("");
+        let splitS = s.split(""); //TC=O(n)
         let size=splitS.length
-        let res = [];
+        let res = [];  //SC=O(n)
 
         let i = 0;
-        while (i < size) {
+        while (i < size) {  //TC=O(n)
             res.push(splitS.pop());
             i++
         }
-        return res.join("");
+        return res.join(""); //TC=O(n)
     }
 }
 
 let str = new Solution();
 let s="GeeksForGeeks"
-console.log(str.reverse(s)) //skeeGrofskee
+console.log(str.reverse(s)) //skeeGroFskeeG
 
+// TC=O(3n)
+// SC=O(n)
