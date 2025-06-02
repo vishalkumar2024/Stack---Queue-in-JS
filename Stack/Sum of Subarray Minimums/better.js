00d0f3167d727229ffcc24c7c2646dfa62014dd1
vishalkumar2024 <vkumarsoni30@gmail.com>
2025-06-02 14:23:50 +0530

    added better solution for sum of subarrays sum folder

---
+++ Stack/Sum of Subarray Minimums/better.js
@@ -0,0 +1,20 @@
+// Leetcode 907 
+
+function sumSubarrayMins(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {  //TC=O(n)
+        let min = arr[i];
+        for (let j = i; j < arr.length; j++) {  //TC=O(n)
+            min = Math.min(min, arr[j])
+            sum += min;
+        }
+    }
+    return sum;
+};
+
+let arr = [3, 1, 2, 2];
+console.log(sumSubarrayMins(arr));
+
+
+// TC=O(n*n)
+// SC=O(n)
--- Stack/Sum of Subarray Minimums/brute-force.js
+++ Stack/Sum of Subarray Minimums/brute-force.js
@@ -2,7 +2,7 @@
 
 function sumSubarrayMins(arr) {
     
-    //function to find the lowest number of each subarrays
+    //function to find the lowest number  
     function lowest(arr) {  
         let low = arr[0]
         for (let i = 0; i < arr.length; i++) {   //TC=O(n)
@@ -13,6 +13,7 @@ function sumSubarrayMins(arr) {
         return low;
     }
 
+    // Finding all Subarrays 
     let res = []
     for (let k = 0; k < arr.length - 1; k++) {  //TC=O(n)
         for (let i = arr.length - 1; i >= k; i--) {  //TC=O(n)
@@ -27,9 +28,9 @@ function sumSubarrayMins(arr) {
     elem.push(arr[arr.length - 1])
     res.push(elem)
 
-
+    // find the lowest number of each subarrays and add them
     let ans = 0
-    for (let i = 0; i < res.length; i++) {  //TC=O(2^n)
+    for (let i = 0; i < res.length; i++) {  //TC=O(n(n+1)/2)
         ans += lowest(res[i])
     }
     return ans
@@ -39,5 +40,5 @@ let arr = [3, 1, 2, 4];
 console.log(sumSubarrayMins(arr));
 
 
-// TC=O(n*n*n)
+// TC=O(n*n*n) + O(n*n*n) ~ O(2(n*n*n))
 // SC=O(n*n)
