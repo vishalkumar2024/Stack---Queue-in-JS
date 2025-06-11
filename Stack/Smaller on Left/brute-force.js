6fc6c878f61ebdc63edf1a85ca12db13d9a67fb3
vishalkumar2024 <vkumarsoni30@gmail.com>
2025-06-11 14:23:13 +0530

    added brute force solution for left smaller on left gfg problem

---
+++ Stack/Smaller on Left/brute-force.js
@@ -0,0 +1,29 @@
+// Given an array arr[] of integers, for each element in the array, find the 
+// nearest smaller element on its left. If there is no such smaller element, 
+// return -1 for that position.
+
+function leftSmaller(arr) {
+    let stk = [];  // SC=O(n)
+    let res = [];  // SC=O(n)
+    for (let i = 0; i < arr.length; i++) {  //TC=O(n)
+        let j = stk.length - 1;
+        while (j >= 0 && stk.length !== 0) {  // TC=O(n)
+            if (stk[j] < arr[i]) {
+                res.push(stk[j])
+                stk.push(arr[i])
+                break;
+            }
+            j--;
+        }
+        if (j == -1) {
+            stk.push(arr[i])
+            res.push(-1)
+        }
+    }
+    return res;
+}
+let arr = [1, 5, 0, 3, 4, 5]
+console.log(leftSmaller(arr));
+
+// TC=O(n*n)
+// SC=O(2n)
